e9bedfb1cec91b268b1cc7509cc38e22f2977fdb
Guiomar 20 <Guiomar 20@LAPTOP-VEALR2IF>
2023-10-23 14:15:50 -0300

    Adicionar arquivos de projeto.

---
+++ Redesocial/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Redesocial.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
